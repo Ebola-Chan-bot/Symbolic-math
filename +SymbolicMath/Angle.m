2402d10e8c03d15e4144a1711b2f22a3f97ce7a5
埃博拉酱 <vhtmfu@outlook.com>
2025-09-10 21:23:46 +0800

    20250910

--- +SymbolicMath/Angle.m
+++ +SymbolicMath/Angle.m
@@ -7,11 +7,31 @@ classdef Angle
 		%从顶点指向角平分线的方向角(-π,π]
 		BisectorAngle(1,1)
 
-		%角度大小
+		%角度大小[0,2π)
 		Radian(1,1)
 	end
 	methods
 		function obj = Angle(Vertex,BisectorAngle,Radian)
+			%# 语法
+			% ```
+			% import SymbolicMath.Angle;
+			%
+			% obj=Angle(ABC);
+			% %给定三点ABC或名称，构造∠ABC
+			%
+			% obj=Angle(B,AC);
+			% %给定顶点B和两条边上两点AC，构造∠ABC
+			%
+			% obj=Angle(Vertex,BisectorAngle,Radian);
+			% %给定顶点坐标、角平分线方向角和角度大小，构造角
+			% ```
+			%# 输入参数
+			% ABC(1,3)SymbolicMath.Point|char，三个点或名称
+			% B(1,1)SymbolicMath.Point，顶点
+			% AC(1,2)SymbolicMath.Point，角两边上两点
+			% Vertex(2,1)，顶点坐标
+			% BisectorAngle(1,1)，从顶点指向角平分线的方向角(-π,π]
+			% Radian(1,1)，角度大小
 			switch nargin
 				case 1
 					if isa(Vertex,'SymbolicMath.Point')
@@ -20,12 +40,19 @@ classdef Angle
 						Vertex=sym(arrayfun(@string,Vertex)+["1";"2"],'real');
 					end
 					obj.Vertex=Vertex(:,2);
-					obj.BisectorAngle=atan2(mean(Vertex(:,[1,3]),2)-obj.Vertex);
-					obj.Radian=
-				case 2
-					obj.Vertex=Vertex.Coordinate;
-					obj.BisectorAngle=atan2(mean([BisectorAngle.Coordinate],2)-obj.Vertex);
-					obj.Radian=mod(atan2(BisectorAngle(1).Coordinate-obj.Vertex)-atan2(BisectorAngle(2).Coordinate-obj.Vertex),2*pi);
+					% 向量 BA 和 BC
+					BA = Vertex(:,1) - Vertex(:,2);
+					BC = Vertex(:,3) - Vertex(:,2);
+
+					% 计算∠ABC的大小
+					normBA = norm(BA);
+					normBC = norm(BC);
+					obj.Radian = acos(dot(BA, BC) / (normBA * normBC)); % 弧度值 [0,pi]
+
+					% 计算角平分线方向向量
+					bisector_vec = BA / normBA + BC / normBC;
+
+					obj.BisectorAngle = atan2(bisector_vec(2), bisector_vec(1)); % (-pi,pi]
 				case 3
 					obj.Vertex=Vertex;
 					obj.BisectorAngle=BisectorAngle;
@@ -33,8 +60,8 @@ classdef Angle
 			end
 		end
 		function SL=Bisector(obj)
-			Angle=mean(obj.Angles)+pi/2;
-			SL=SymbolicMath.StraightLine(Angle,obj.Vertex(1).*cos(Angle) + obj.Vertex(2).*sin(Angle));
+			Angle = atan2(sin(obj.BisectorAngle+pi/2), cos(obj.BisectorAngle+pi/2));
+			SL=SymbolicMath.StraightLine(Angle,obj.Vertex(1)*cos(Angle) + obj.Vertex(2)*sin(Angle));
 		end
 	end
 end
--- +SymbolicMath/Arc.m
+++ +SymbolicMath/Arc.m
@@ -7,13 +7,13 @@ classdef Arc
 		Radius(1,1)
 
 		%从圆心指向弧中点的方向角(-π,π]
-		MidpointAngle(1,1)
+		DirectionAngle(1,1)
 
 		%弧对应的圆心角[0,2π)
-		SectorAngle(1,1) 
+		CentralAngle(1,1)
 	end
 	methods
-		function obj = Arc(Center,Radius,MidpointAngle,SectorAngle)
+		function obj = Arc(Center,Radius,DirectionAngle,CentralAngle)
 			switch nargin
 				case 2
 					obj.Center=Center.Center;
@@ -23,14 +23,34 @@ classdef Arc
 					else
 						Radius=sym(arrayfun(@string,Radius)+["1";"2"],'real');
 					end
-					
-					obj.MidpointAngle=atan2(Radius(:,2)-obj.Center);
-					obj.SectorAngle=mod(atan2(Radius(1,:)-obj.Center)-atan2(Radius(3,:)-obj.Center),2*pi);
+					uABC=Radius-Center.Center;
+					uABC=uABC./vecnorm(uABC,2,1); % 转成单位向量
+
+					% 有符号角（-pi, pi]
+					angAC_signed = atan2( det(uABC(:,[1,3])) , dot(uABC(:,1),uABC(:,3)) );
+					angAB_signed = atan2( det(uABC(:,[1,2])) , dot(uABC(:,1),uABC(:,2)) );
+
+					% 方向因子: 1 表示沿 angAC_signed方向，-1 表示反向
+					dirFactor = 1 - 2 * ( abs(angAB_signed) > abs(angAC_signed) );
+
+					% 经过B的有符号弧角
+					ang_viaB = angAC_signed * dirFactor;
+
+					% 圆心角 [0, 2*pi)
+					tmpAng = atan2(sin(ang_viaB), cos(ang_viaB)); % (-pi, pi]
+					arcAngle = tmpAng + (tmpAng < 0) * 2*pi;      % 转到 [0, 2*pi)
+
+					% 起点方向角
+					angA = atan2(uA(2), uA(1));
+
+					% 弧中点方向角 (-pi, pi]
+					midDir = atan2( sin(angA + ang_viaB/2), cos(angA + ang_viaB/2) );
+
 				case 4
 					obj.Center = Center;
 					obj.Radius = Radius;
-					obj.MidpointAngle = MidpointAngle;
-					obj.SectorAngle = SectorAngle;
+					obj.DirectionAngle = DirectionAngle;
+					obj.CentralAngle = CentralAngle;
 			end
 		end
 	end
